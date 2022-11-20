31449176a89f5c84c0c59f838ab0b4e1af684968
Philippe SABAA <philippe.sabaa@insee.fr>
2022-11-20 22:26:59 +0100

    Ajout correction simple sans Matcher exo 1

--- tests-unitaires-exercices/exercice01/src/test/java/fr/insee/exemple/services/ModeleVoitureServicesTest.java
+++ tests-unitaires-exercices/exercice01/src/test/java/fr/insee/exemple/services/ModeleVoitureServicesTest.java
@@ -1,8 +1,8 @@
 package fr.insee.exemple.services;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,19 +18,24 @@ public class ModeleVoitureServicesTest {
 	@Test
 	public void filtrerModelesMoinsPolluantsTest() {
 		// GIVEN
-		final ModeleVoiture citroenSaxoEssence1995 = new ModeleVoiture("Citroen", "Saxo", Carburant.ESSENCE,	 	LocalDate.of(1995, 01, 01));
-		final ModeleVoiture peugeot405Diesel2000 = new ModeleVoiture("Peugeot", "405",  Carburant.DIESEL,  	LocalDate.of(2000, 01, 01));
-		final ModeleVoiture renaultClioEssence1999 = new ModeleVoiture("Renault", "Clio", Carburant.ESSENCE, 	LocalDate.of(1999, 01, 01));
-		final ModeleVoiture citroenSaxoElectrique2015 = new ModeleVoiture("Citroen", "Saxo", Carburant.ELECTRIQUE, 	LocalDate.of(2015, 01, 01));
-		final ModeleVoiture renaultClioGpl2013 = new ModeleVoiture("Renault", "Clio", Carburant.GPL, 		LocalDate.of(2013, 01, 01));
-		
+		final ModeleVoiture citroenSaxoEssence1995 = new ModeleVoiture("Citroen", "Saxo", Carburant.ESSENCE,
+				LocalDate.of(1995, 01, 01));
+		final ModeleVoiture peugeot405Diesel2000 = new ModeleVoiture("Peugeot", "405", Carburant.DIESEL,
+				LocalDate.of(2000, 01, 01));
+		final ModeleVoiture renaultClioEssence1999 = new ModeleVoiture("Renault", "Clio", Carburant.ESSENCE,
+				LocalDate.of(1999, 01, 01));
+		final ModeleVoiture citroenSaxoElectrique2015 = new ModeleVoiture("Citroen", "Saxo", Carburant.ELECTRIQUE,
+				LocalDate.of(2015, 01, 01));
+		final ModeleVoiture renaultClioGpl2013 = new ModeleVoiture("Renault", "Clio", Carburant.GPL,
+				LocalDate.of(2013, 01, 01));
+
 		List<ModeleVoiture> modeles = new ArrayList<>();
-		modeles.add(citroenSaxoEssence1995 );
-		modeles.add(peugeot405Diesel2000 );
-		modeles.add(renaultClioEssence1999 );
-		modeles.add(citroenSaxoElectrique2015 );
-		modeles.add(renaultClioGpl2013 );
-		
+		modeles.add(citroenSaxoEssence1995);
+		modeles.add(peugeot405Diesel2000);
+		modeles.add(renaultClioEssence1999);
+		modeles.add(citroenSaxoElectrique2015);
+		modeles.add(renaultClioGpl2013);
+
 		System.out.println("Liste des modèles avant filtrage");
 		for (ModeleVoiture modeleVoiture : modeles) {
 			System.out.println(modeleVoiture);
@@ -39,19 +44,21 @@ public class ModeleVoitureServicesTest {
 		// WHEN
 		ModeleVoitureServices service = new ModeleVoitureServices();
 		List<ModeleVoiture> voituresMoinsPolluantes = service.filtrerModelesMoinsPolluants(modeles);
-		
+
 		// THEN
 		System.out.println("Liste des modèles après filtrage");
 		for (ModeleVoiture modeleVoiture : voituresMoinsPolluantes) {
 			System.out.println(modeleVoiture);
 		}
-		
-	
+
 		// modeles attendus après filtrage
-		
-		assertThat(voituresMoinsPolluantes, 
-				containsInAnyOrder(renaultClioEssence1999, citroenSaxoElectrique2015, renaultClioGpl2013)
-				);
+		assertTrue(voituresMoinsPolluantes.contains(renaultClioEssence1999));
+		assertTrue(voituresMoinsPolluantes.contains(citroenSaxoElectrique2015));
+		assertTrue(voituresMoinsPolluantes.contains(renaultClioGpl2013));
+
+		// Avec les Matcher (cf partie suivante du diapo)
+		assertThat(voituresMoinsPolluantes,
+				containsInAnyOrder(renaultClioEssence1999, citroenSaxoElectrique2015, renaultClioGpl2013));
 	}
-	
+
 }
