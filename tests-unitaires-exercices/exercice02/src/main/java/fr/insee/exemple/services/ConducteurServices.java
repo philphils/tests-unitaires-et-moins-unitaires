dc751479442c6d0bc55a15d35b99e260a770be05
Philippe SABAA <philippe.sabaa@insee.fr>
2022-11-21 18:45:17 +0100

    Ajout correction exo2 sans Java 8

--- tests-unitaires-exercices/exercice02/src/main/java/fr/insee/exemple/services/ConducteurServices.java
+++ tests-unitaires-exercices/exercice02/src/main/java/fr/insee/exemple/services/ConducteurServices.java
@@ -1,28 +1,59 @@
 package fr.insee.exemple.services;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import fr.insee.exemple.model.Conducteur;
 import fr.insee.exemple.model.ModeleVoiture;
 import fr.insee.exemple.model.Voiture;
 
 public class ConducteurServices {
-	
+
 	private ModeleVoitureServices modeleVoitureServices;
-	
+
 	public ConducteurServices(ModeleVoitureServices modeleVoitureServices) {
 		this.modeleVoitureServices = modeleVoitureServices;
 	}
 
-	private boolean estUnConducteurPossedantAuMoinsUnModelePeuPolluant(Conducteur conducteur) {
-		List<ModeleVoiture> modelesPossedesParConducteur = conducteur.getVoitures().stream().map(Voiture::getModele).collect(Collectors.toList());
-		return !modeleVoitureServices.filtrerModelesMoinsPolluants(modelesPossedesParConducteur).isEmpty();
-	}
-	
+	/**
+	 * Correction sans Java 8
+	 */
 	public List<Conducteur> filtrerConducteursPossedantAuMoinsUnModelePeuPolluant(List<Conducteur> conducteurs) {
-		return conducteurs.stream()
-				.filter(this::estUnConducteurPossedantAuMoinsUnModelePeuPolluant)
-				.collect(Collectors.toList());
+
+		List<Conducteur> conducteursResult = new ArrayList<>();
+
+		// Correction 1 : Sans Java 8
+		for (Conducteur conducteur : conducteurs) {
+
+			List<ModeleVoiture> modeleVoitures = new ArrayList<>();
+
+			for (Voiture voiture : conducteur.getVoitures()) {
+
+				modeleVoitures.add(voiture.getModele());
+
+			}
+
+			if (modeleVoitureServices.filtrerModelesMoinsPolluants(modeleVoitures).size() > 0) {
+				conducteursResult.add(conducteur);
+			}
+
+		}
+
+		return conducteursResult;
+
 	}
+
+	/**
+	 * Correction avec Java 8
+	 */
+//	private boolean estUnConducteurPossedantAuMoinsUnModelePeuPolluant(Conducteur conducteur) {
+//		List<ModeleVoiture> modelesPossedesParConducteur = conducteur.getVoitures().stream().map(Voiture::getModele).collect(Collectors.toList());
+//		return !modeleVoitureServices.filtrerModelesMoinsPolluants(modelesPossedesParConducteur).isEmpty();
+//	}
+//	
+//	public List<Conducteur> filtrerConducteursPossedantAuMoinsUnModelePeuPolluant(List<Conducteur> conducteurs) {
+//		return conducteurs.stream()
+//				.filter(this::estUnConducteurPossedantAuMoinsUnModelePeuPolluant)
+//				.collect(Collectors.toList());
+//	}
 }
