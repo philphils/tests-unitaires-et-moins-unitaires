bdc4a77e81d488567300ed7914d6eeb8b5544514
Philippe SABAA <philippe.sabaa@insee.fr>
2022-11-23 13:50:13 +0100

    Nettoyage correction exo 1

--- tests-unitaires-exercices/exercice04/src/test/java/fr/insee/exemples/services/ModeleVoitureServicesTest.java
+++ tests-unitaires-exercices/exercice04/src/test/java/fr/insee/exemples/services/ModeleVoitureServicesTest.java
@@ -1,7 +1,7 @@
 package fr.insee.exemples.services;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,48 +19,62 @@ import fr.insee.exemple.services.ModeleVoitureServices;
 public class ModeleVoitureServicesTest {
 
 	private ModeleVoitureServices mvs = new ModeleVoitureServices();
-	
+
 	// cf. solution 3 sur assertions multiples
 	@Rule
 	public ErrorCollector collector = new ErrorCollector();
-	
+
 	@Test
 	public void testerFiltrerModelesMoinsPolluants() {
 		// GIVEN
 		List<ModeleVoiture> modeles = new ArrayList<>();
-		modeles.add(new ModeleVoiture("Citroen", "Saxo", Carburant.ESSENCE,	 	DateUtils.asDate(LocalDate.of(1995, 01, 01))));
-		modeles.add(new ModeleVoiture("Peugeot", "405",  Carburant.DIESEL,  	DateUtils.asDate(LocalDate.of(2000, 01, 01))) );
+		modeles.add(
+				new ModeleVoiture("Citroen", "Saxo", Carburant.ESSENCE, DateUtils.asDate(LocalDate.of(1995, 01, 01))));
+		modeles.add(
+				new ModeleVoiture("Peugeot", "405", Carburant.DIESEL, DateUtils.asDate(LocalDate.of(2000, 01, 01))));
 
-		ModeleVoiture modeleMoinsPolluant1 = new ModeleVoiture("Renault", "Clio", Carburant.ESSENCE, 	DateUtils.asDate(LocalDate.of(1999, 01, 01)));
+		ModeleVoiture modeleMoinsPolluant1 = new ModeleVoiture("Renault", "Clio", Carburant.ESSENCE,
+				DateUtils.asDate(LocalDate.of(1999, 01, 01)));
 		modeles.add(modeleMoinsPolluant1);
-		ModeleVoiture modeleMoinsPolluant2 = new ModeleVoiture("Renault", "Clio", Carburant.DIESEL, 	DateUtils.asDate(LocalDate.of(2015, 01, 01)));
+		ModeleVoiture modeleMoinsPolluant2 = new ModeleVoiture("Renault", "Clio", Carburant.DIESEL,
+				DateUtils.asDate(LocalDate.of(2015, 01, 01)));
 		modeles.add(modeleMoinsPolluant2);
-		ModeleVoiture modeleMoinsPolluant3 = new ModeleVoiture("Citroen", "Saxo", Carburant.ELECTRIQUE, 	DateUtils.asDate(LocalDate.of(2015, 01, 01)));
+		ModeleVoiture modeleMoinsPolluant3 = new ModeleVoiture("Citroen", "Saxo", Carburant.ELECTRIQUE,
+				DateUtils.asDate(LocalDate.of(2015, 01, 01)));
 		modeles.add(modeleMoinsPolluant3);
-		ModeleVoiture modeleMoinsPolluant4 = new ModeleVoiture("Renault", "Clio", Carburant.GPL, 		DateUtils.asDate(LocalDate.of(2013, 01, 01)));
+		ModeleVoiture modeleMoinsPolluant4 = new ModeleVoiture("Renault", "Clio", Carburant.GPL,
+				DateUtils.asDate(LocalDate.of(2013, 01, 01)));
 		modeles.add(modeleMoinsPolluant4);
-		
+
 		System.out.println("Liste des modèles avant filtrage");
 		for (ModeleVoiture modeleVoiture : modeles) {
 			System.out.println(modeleVoiture);
 		}
 
 		List<ModeleVoiture> modelesPeuPolluants = mvs.filtrerModelesMoinsPolluants(modeles);
-		
+
 		System.out.println("Liste des modèles après filtrage");
 		for (ModeleVoiture modeleVoiture : modelesPeuPolluants) {
 			System.out.println(modeleVoiture);
 		}
 
-		// Solution 1		
-		//assertThat("contient tous les moins polluants", modelesPeuPolluants, hasItems(modeleMoinsPolluant1, modeleMoinsPolluant2, modeleMoinsPolluant3, modeleMoinsPolluant4));
-		//assertThat("contient exactement 4 éléments", modelesPeuPolluants.size(), equalTo(4));
-		
+		// Solution 1
+		// assertThat("contient tous les moins polluants", modelesPeuPolluants,
+		// hasItems(modeleMoinsPolluant1, modeleMoinsPolluant2, modeleMoinsPolluant3,
+		// modeleMoinsPolluant4));
+		// assertThat("contient exactement 4 éléments", modelesPeuPolluants.size(),
+		// equalTo(4));
+
 		// Solution 2
-		//assertThat("contient exactement tous les moins polluants", modelesPeuPolluants, allOf(hasItems(modeleMoinsPolluant1, modeleMoinsPolluant2, modeleMoinsPolluant3, modeleMoinsPolluant4), hasSize(4)));
-		
-		// Solution 3 - cf. https://carminetech.wordpress.com/2015/10/30/junit-how-to-collect-multiple-assertion-results-in-one-test/
-		collector.checkThat(modelesPeuPolluants, hasItems(modeleMoinsPolluant1, modeleMoinsPolluant2, modeleMoinsPolluant3, modeleMoinsPolluant4));
+		// assertThat("contient exactement tous les moins polluants",
+		// modelesPeuPolluants, allOf(hasItems(modeleMoinsPolluant1,
+		// modeleMoinsPolluant2, modeleMoinsPolluant3, modeleMoinsPolluant4),
+		// hasSize(4)));
+
+		// Solution 3 - cf.
+		// https://carminetech.wordpress.com/2015/10/30/junit-how-to-collect-multiple-assertion-results-in-one-test/
+		collector.checkThat(modelesPeuPolluants,
+				hasItems(modeleMoinsPolluant1, modeleMoinsPolluant2, modeleMoinsPolluant3, modeleMoinsPolluant4));
 		collector.checkThat(modelesPeuPolluants.size(), equalTo(4));
 	}
 }
